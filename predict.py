4692a64d17e2289d9bdffb3b860323a771c153af
Muaaz Shaikh <41267142+mizerablepi@users.noreply.github.com>
2022-10-27 13:08:46 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,4 +1,6 @@
 import pickle
+import warnings
+warnings.simplefilter("ignore", UserWarning)# Ignore warnings caused due to absence of input column name
 
 def model_prediction(features):
     pickled_model = pickle.load(open('./Model/TSLA_stock_prediction.pkl', 'rb'))
@@ -6,6 +8,6 @@ def model_prediction(features):
     
     return stock_price
   
-test_features = [[17.114,17.344667,75346500]] #Passed in the order 'Open','Close','Volume'
+test_features = [[17.114,17.344667,75346500]] #Passed in the order 'Open','Close','Volume' respectively
 
 print(f'The stock price the next day will be ${model_prediction(test_features)}')
